0e3dd73e594b3d36e3522872fe8e6921ffabf4da
Thierry Stofel <137530556+ThierrySTFL@users.noreply.github.com>
2024-01-10 12:32:07 -0300

    adding the preview system for the numbers

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,6 +11,32 @@ public partial class MainPage : ContentPage
 	{
 		NameApp.IsVisible = false;
 		ContainerLuckNumbers.IsVisible = true;
+
+		GenerateLuckNumbers(sender, e);
 	}
+
+	private void GenerateLuckNumbers(object sender, EventArgs e)
+	{
+        var random = new Random();
+        var luckNumbers = new List<int>();
+
+        while (luckNumbers.Count < 6)
+		{
+            var number = random.Next(1, 61);
+
+            if (!luckNumbers.Contains(number))
+			{
+                luckNumbers.Add(number);
+            }
+        }
+
+        n1.Text = luckNumbers[0].ToString();
+		n2.Text = luckNumbers[1].ToString();
+		n3.Text = luckNumbers[2].ToString();
+		n4.Text = luckNumbers[3].ToString();
+		n5.Text = luckNumbers[4].ToString();
+		n6.Text = luckNumbers[5].ToString();
+        
+    }
 
 }
